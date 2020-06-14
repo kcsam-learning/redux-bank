24de130f8b36482c0c744c94df4eb557aeb3c9a9
samkahchiin <samkahchiin@gmail.com>
2020-06-14 19:52:19 +0800

    [Chore] Added constants for action type

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+const constants = {
+  WITHDRAW_MONEY: 'WITHDRAW_MONEY',
+  ALL_OUT_DONATION: 'ALL_OUT_DONATION'
+}
+
+export default constants;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
+import C from '../constants';
+
 export const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
-    case 'WITHDRAW_MONEY':
+    case C.WITHDRAW_MONEY:
       return {
         ...state,
         totalAmount: state.totalAmount - action.amount
       }
-    case 'ALL_OUT_DONATION':
+    case C.ALL_OUT_DONATION:
       return {
         ...state,
         totalAmount: 0
